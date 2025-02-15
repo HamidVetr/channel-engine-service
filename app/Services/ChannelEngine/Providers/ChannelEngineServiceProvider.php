0fa851600256b9ec02df1b148e2b314fed82c25b
Hamid Vetr <hamid.vetr@endeavour.nl>
2025-02-15 12:32:14 +0100

    register RequestFactory and ChannelEngineResponseFactory in ChannelEngineServiceProvider

--- app/Services/ChannelEngine/Providers/ChannelEngineServiceProvider.php
+++ app/Services/ChannelEngine/Providers/ChannelEngineServiceProvider.php
@@ -10,7 +10,10 @@ use ChannelEngine\Contracts\ChannelEngineClientInterface;
 use ChannelEngine\Contracts\ChannelEngineServiceInterface;
 use ChannelEngine\Contracts\Factories\ChannelEngineResponseFactoryInterface;
 use ChannelEngine\Contracts\Factories\RequestFactoryInterface;
+use ChannelEngine\Factories\ChannelEngineResponseFactory;
+use ChannelEngine\Factories\RequestFactory;
 use GuzzleHttp\Client as GuzzleClient;
+use Illuminate\Contracts\Config\Repository as Config;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,12 +23,29 @@ class ChannelEngineServiceProvider extends ServiceProvider
     {
         $this->registerServices()
             ->registerClients()
-            ->registerCommands();
+            ->registerCommands()
+            ->registerFactories();
     }
 
     protected function registerServices(): self
     {
         $this->app->singleton(ChannelEngineServiceInterface::class, ChannelEngineService::class);
+
+        return $this;
+    }
+
+    protected function registerFactories(): self
+    {
+        $this->app->singleton(RequestFactoryInterface::class, function (Container $container) {
+            $config = $container->make(Config::class);
+
+            return new RequestFactory(
+                apiKey : $config->get('channel-engine.api.key'),
+                baseUri: $config->get('channel-engine.api.base_url') . '/' . $config->get('channel-engine.api.version'),
+            );
+        });
+
+        $this->app->singleton(ChannelEngineResponseFactoryInterface::class, ChannelEngineResponseFactory::class);
 
         return $this;
     }
