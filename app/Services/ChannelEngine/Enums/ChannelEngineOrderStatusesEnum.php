b17da69886a202b5ac74da693652a16360511095
Hamid Vetr <hamid.vetr@endeavour.nl>
2025-02-15 13:06:03 +0100

    create ChannelEngineOrderStatusesEnum

---
+++ app/Services/ChannelEngine/Enums/ChannelEngineOrderStatusesEnum.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChannelEngine\Enums;
+
+enum ChannelEngineOrderStatusesEnum
+{
+    case IN_PROGRESS;
+    case SHIPPED;
+    case IN_BACKORDER;
+    case MANCO;
+    case CANCELLED;
+    case IN_COMBI;
+    case CLOSED;
+    case NEW;
+    case RETURNED;
+    case REQUEST_CORRECTION;
+    case AWAITING_PAYMENT;
+}
